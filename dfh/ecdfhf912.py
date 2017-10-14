bcd372b72486cb292375b7eacf4d2d838ca83498
Wesley Felipe <wesleys.felipe@gmail.com>
2017-10-14 19:15:13 -0300

    Avaliação Penteado

--- dfh/ecdfhf912.py
+++ dfh/ecdfhf912.py
@@ -3,13 +3,14 @@ import numpy as np
 
 from dfh.itens.penteado.classificador import classificarPenteado
 
-pontoDeCorte = 0.5
-tolerancia = 0.05
+pontoDeCorteGlobal = 0.5
+toleranciaGlobal = 0.05
 
 class ResultadoItem:
-	resultadoClassificacao = None
-	resultadoNormatizado = None
-	peso = None	
+	resultadoClassificacao = None #resultado emitido pelo pela ConvNet
+	resultadoAvaliado = None #A para ausente, P para presente, I para indeterminado
+	pesoItem = None # peso deste item para a composição na composição da escala
+	escore = None # resultado final, quanto somar no escore final do desenho avaliado 
 
 class EcDfhF912Results:
 	adaptacaoCabelo = None
@@ -30,28 +31,34 @@ def classificarEcDfhF912(path):
 		
 def classificarItens(img):
 	results = EcDfhF912Results()
-	penteado = ResultadoItem()
-	penteado.resultadoClassificacao = avaliarPenteado(img)
-	results.penteado = penteado
-	
+	results.penteado = avaliarPenteado(img)
 	return results
 	
+def avaliarPenteado(img):
+	penteado = ResultadoItem()
+	penteado.resultadoClassificacao = classificarPenteado(img)
+	penteado.resultadoAvaliado = avaliarResultado(penteado.resultadoClassificacao, pontoDeCorteGlobal, toleranciaGlobal)
+	penteado.pesoItem = 3
+	
+	if(penteado.resultadoAvaliado == 'A'):
+		penteado.escore = penteado.pesoItem
+	else:
+		penteado.escore = 0
+		
+	return penteado
+
 def prepararImagem(path):
 	img = cv2.imread(path)
 	img = cv2.resize(img,(299,299))
 	img = np.reshape(img,[1,299,299,3])
 	return img
-	
-def avaliarPenteado(img):
-	return avaliarResultado(classificarPenteado(img))
-
 		
-def avaliarResultado(result):
+def avaliarResultado(result, pontoDeCorte, tolerancia):
 	if(result >= pontoDeCorte + tolerancia):
-		return 1.0
+		return 'P'
 	elif(result <= pontoDeCorte - tolerancia):
-		return 0.0
+		return 'A'
 	else:
-		return -1.0
+		return 'I'
 	
 	
